b1af866f405caf8e0914e60ba98e007f30746800
Waylon Dixon <wayjay4@yahoo.com>
2024-01-27 17:16:09 -0700

    fixed bug in pagination links, Videos component not recieving videos data

--- resources/js/Pages/VideosPaginationLinks.jsx
+++ resources/js/Pages/VideosPaginationLinks.jsx
@@ -2,13 +2,11 @@ import VideoData from "@/Pages/Videos/VideoData.jsx";
 import PaginationLinks from "@/Components/PaginationLinks.jsx";
 
 function VideosPaginationLinks(props) {
-    console.log(props);
-
     return (
         <div className={'video-catalog-container'}>
             <div className="mt-6">
                 <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-                    <VideoData video={props['videos']} />
+                    <VideoData videos={props['videos'].data} />
                 </ul>
             </div>
 
