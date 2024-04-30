d097c189b57588f059853a308caac4cdc4b30aaf
Waylon Dixon <wd@signingdaysports.com>
2024-04-29 23:16:45 -0700

    set up procedure to store random video images to be used in paginator to clientside local storage

--- resources/js/Pages/Videos/Components/VideoData.jsx
+++ resources/js/Pages/Videos/Components/VideoData.jsx
@@ -6,18 +6,30 @@ function VideoData({videos}) {
         return (date) ? formatDistance(new Date(date), new Date(), {addSuffix: true}) : '';
     }
 
+    // if not already, store random images to local storage to be used for video images
+    if(!localStorage.getItem('images')) {
+        const images_arr = [];
+        for (let i = 0; i < 300; i++) {
+            images_arr.push(useThumbnails().generateThumbnail());
+        }
+        localStorage.setItem('images', JSON.stringify(images_arr));
+    }
+    const images = JSON.parse(localStorage.getItem('images'));
+
     return (
         <>
-            {videos.map(video=> (
-                <li key={video.id} className="video relative">
-                    <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:offset-2 focus-within:ring-offset-gray-100">
-                        <img src={useThumbnails().generateThumbnail()} alt={'video_thumbnail'} className="pointer-events-none object-cover group-hover:opacity-75" />
-                    </div>
-                    <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{video.title}</p>
-                    <p className="pointer-events-none line-clamp-3 text-sm text-gray-500">{video.description}</p>
-                    <p className="mt-1 pointer-events-none block text-sm font-medium text-gray-500">Created by <span className="text-gray-600 font-bold">{video.user.name}</span> {computeReadableDate(video.created_at)} ID:{video.id}</p>
-                </li>
-            ))}
+            {videos.map((video, index) => {
+                return (
+                    <li key={video.id} className="video relative">
+                        <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:offset-2 focus-within:ring-offset-gray-100">
+                            <img src={images[video.id]} alt={'video_thumbnail'} className="pointer-events-none object-cover group-hover:opacity-75" />
+                        </div>
+                        <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{video.title}</p>
+                        <p className="pointer-events-none line-clamp-3 text-sm text-gray-500">{video.description}</p>
+                        <p className="mt-1 pointer-events-none block text-sm font-medium text-gray-500">Created by <span className="text-gray-600 font-bold">{video.user.name}</span> {computeReadableDate(video.created_at)} ID:{video.id}</p>
+                    </li>
+                );
+            })}
         </>
     )
 }
